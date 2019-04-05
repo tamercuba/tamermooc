a6325152a616a9e84810e7858f6e96162eb9f691
tamercuba <tamercuba@gmail.com>
2019-04-05 11:01:45 -0300

    COURSES/URLS

--- courses/urls.py
+++ courses/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<slug:slug>/', views.details , name='details'),
     path('<slug:slug>/inscricao/', views.enrollment, name='enrollment'),
     path('<slug:slug>/anuncios/', views.announcements, name='announcements'),
+    path('<slug:slug>/cancelar-inscricao', views.undo_enrollment ,name='undo_enrollment' ),
 ]
